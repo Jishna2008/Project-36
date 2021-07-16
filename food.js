099433f15d7823b076f9176cdb3bfd66339fa24d
JISHNA <77768080+Jishna2008@users.noreply.github.com>
2021-07-16 22:39:06 +0530

    Add files via upload

---
+++ food.js
@@ -0,0 +1,29 @@
+class Food{
+    constructor(){
+        this.image=loadImage("Images/Milk.png");
+      
+    }
+    getFood(){
+        var foodRef=database.ref('food');
+        foodRef.on("value",function (data){foodS=data.val();})
+    }
+    updateFood(foodS){
+        database.ref('/').update({'food':foodS})
+    }
+    display(){
+        var x=80,y=100;
+        imageMode(CENTER)
+        image(this.image,720,220,70,70);
+     for(var i;i<=200;i=i+20)
+     {
+         x=x+20;
+         y=y;
+         image(this.image,x,y,70,70);
+     }
+  
+   
+    }
+    updateName(petName){
+        database.ref('Pet/name').update({name:petName })
+    }
+}
